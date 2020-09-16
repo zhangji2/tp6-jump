7275184b07017702c0dece7690e3a98b1a83ff4e
zhangji <zhangji2@qq.com>
2020-09-16 20:57:43 +0800

    fix config info

--- src/config/jump.php
+++ src/config/jump.php
@@ -2,5 +2,5 @@
 return[
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl' => app()->getRootPath().'/vendor/zhangji2/tp6-jump/src/view/jump.html',
-    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/zhangji2/tp6-jump/src/view/ump.html',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/zhangji2/tp6-jump/src/view/jump.html',
 ];
