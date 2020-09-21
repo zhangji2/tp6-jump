dc745179bfb54f8c67d69c940295c30d3d21e5bc
zhangji <zhangji2@qq.com>
2020-09-21 22:14:06 +0800

    filename

--- src/config/jump.php
+++ src/config/jump.php
@@ -1,6 +1,6 @@
 <?php
 return[
     // 默认跳转页面对应的模板文件
-    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/zhangji2/tp6-jump/src/view/jump.html',
-    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/zhangji2/tp6-jump/src/view/jump.html',
+    'dispatch_success_tmpl' => app()->getRootPath().'vendor/zhangji2/tp6-jump/src/view/jump.html',
+    'dispatch_error_tmpl'   => app()->getRootPath().'vendor/zhangji2/tp6-jump/src/view/jump.html',
 ];
